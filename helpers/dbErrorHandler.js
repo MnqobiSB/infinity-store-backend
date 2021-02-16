61999c5b5374da0f070a5e4e161a7d6fe0783eb4
MnqobiSB <amandlamm1@gmail.com>
2021-02-16 18:31:41 +0200

    setup category model, route and controller

--- helpers/dbErrorHandler.js
+++ helpers/dbErrorHandler.js
@@ -39,7 +39,7 @@ exports.errorHandler = (error) => {
 	} else {
 		for (let errorName in error.errorors) {
 			if (error.errorors[errorName].message)
-				message = error.errorors[errorName].message;
+				message = error.errors[errorName].message;
 		}
 	}
 

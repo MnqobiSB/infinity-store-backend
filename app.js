cda3d9e930ac0db05fbf0a12bfe26c236f8d2c49
MnqobiSB <amandlamm1@gmail.com>
2021-03-03 07:19:35 +0200

    Backend complete

--- app.js
+++ app.js
@@ -3,14 +3,16 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const expressValidator = require('express-validator');
 require('dotenv').config();
-
 // import routes
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
-const brandRoutes = require('./routes/brand');
+const productRoutes = require('./routes/product');
+const braintreeRoutes = require('./routes/braintree');
+const orderRoutes = require('./routes/order');
 
 // app
 const app = express();
@@ -19,24 +21,25 @@ const app = express();
 mongoose
 	.connect(process.env.DATABASE, {
 		useNewUrlParser: true,
-		useCreateIndex: true,
-		useUnifiedTopology: true
+		useUnifiedTopology: true,
+		useCreateIndex: true
 	})
-	.then(() => {
-		console.log('DB Connected');
-	});
+	.then(() => console.log('DB Connected'));
 
 // middlewares
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(expressValidator());
+app.use(cors());
 
 // routes middleware
 app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
-app.use('/api', brandRoutes);
+app.use('/api', productRoutes);
+app.use('/api', braintreeRoutes);
+app.use('/api', orderRoutes);
 
 const port = process.env.PORT || 8000;
 
